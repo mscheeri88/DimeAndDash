f992813cd9c26888cf87140d27de5da56201b2b2
markwalkernz <markwalkernz@gmail.com>
2017-12-09 13:46:51 -0600

    added package.json

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use("/", routes);
+
+app.listen(port);
