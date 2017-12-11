049ca003ac0d0855b5faf0df2d5eb722e2225817
markwalkernz <markwalkernz@gmail.com>
2017-12-11 14:25:10 -0600

    added database schema

--- db/dimeanddash schema.sql
+++ db/dimeanddash schema.sql
@@ -1,12 +1,8 @@
+-- drop database if exists dimeanddash_db;
+
 create database dimeanddash_db;
 
 use dimeanddash_db;
-
-create table customer (
-	id integer auto_increment not null,
-    venmo_handle varchar(100) not null,
-    primary key (id)
-);
 
 create table customer (
 	customer_id integer auto_increment not null,
@@ -14,18 +10,30 @@ create table customer (
     primary key (customer_id)
 );
 
-create table bill_item (
-	id integer auto_increment not null,
-    description varchar(100) not null,
-    price decimal(7,2),
-    tax_percent decimal(3,2),
-    assigned_to integer,
-    foreign key (assigned_to)
-		references customer(id)
-        on delete cascade,
-    primary key (id)
+create table bill (
+	bill_id integer auto_increment not null,
+    primary key (bill_id)
 );
 
-select * from customer;
+create table menu_item (
+	menu_item_id integer auto_increment not null,
+    description varchar(100) not null,
+    category varchar(100) not null,
+    price decimal(7,2) not null,
+    tax_percent decimal(3,2) not null,
+    primary key (menu_item_id)
+);
 
-select * from bill_item;
+create table bill_item (
+	bill_item_id integer auto_increment not null,
+    menu_item_id integer,
+		foreign key (menu_item_id)
+		references menu_item(menu_item_id),
+	bill_id integer,
+		foreign key (bill_id)
+		references bill(bill_id),
+	customer_id integer,
+		foreign key (customer_id)
+		references customer(customer_id),
+    primary key (bill_item_id)
+);
