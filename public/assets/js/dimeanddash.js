05f128a0e9da06bc5496d2608f7af44a778d19c2
vanarothorn <vanarothorn01@gmail.com>
2017-12-15 01:12:17 -0600

    added drog and drop function

--- public/assets/js/dimeanddash.js
+++ public/assets/js/dimeanddash.js
@@ -1,3 +1,12 @@
+
+$(document).ready(function(){
+  $(".main-item li").draggable({helper:"clone"});
+
+  $(".user-items").droppable({drop:function(event,ui){
+    $("#test").append($("<li></li>").text(ui.draggable.text()));
+  }});
+});
+
 console.log("hello world");
 
 $(".move-button").click (function(){
