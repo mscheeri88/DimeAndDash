e4eef10330a7c11c791efdac704443db3e5025b9
markwalkernz <markwalkernz@gmail.com>
2017-12-15 15:19:07 -0600

    separate index and split-bill js files

---
+++ public/assets/js/index.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+  $(".main-item li").draggable({helper:"clone"});
+
+  $(".user-items").droppable({drop:function(event,ui){
+    $("#test").append($("<li></li>").text(ui.draggable.text()));
+  }});
+
+
+	console.log("index.js found");
+
+	// Index Page ==================================================================
+
+	// click on customer button on index page
+	$(".customer-button").click (function(){
+	    console.log("Customer button was clicked.");
+
+	    // get parameters from billID input box and customer button value
+	    var billID = $("#billID").val();
+	    var numCustomers = $(this).attr("value");
+
+	    // go to the split-bill page, including parameters
+	    var parameters = "cust=" + numCustomers + "&billID=" + billID;
+	    location.href = "/split-bill/" + parameters;
+	});
+
+
+	// Split-Bill Page ==============================================================
+
+	$("#submitVenmo").click (function(){
+		event.preventDefault();
+
+		var numCustomers = $(".customers").attr("data-customers");
+
+		
+
+		console.log("numCustomers: " + numCustomers);
+
+	});
+
+
+	$(".move-button").click (function(){
+	    console.log("Move button was clicked.");
+	});
+
+}); // end of document.ready function
